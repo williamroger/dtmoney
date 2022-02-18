972e9535d2ea251d99e5eddc63d1f3d414d97daa
William Roger <williamroger.frontend@gmail.com>
2022-02-18 13:28:58 -0300

    adicionado componentes TransactionsTable

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,30 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>21/02/2022</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$ 1.100</td>
+            <td>Casa</td>
+            <td>21/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
