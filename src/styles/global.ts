24df0474e295ff1cdc87b8d8f8fe613c58ae002c
William Roger <williamroger.frontend@gmail.com>
2022-02-18 12:10:39 -0300

    adicionado componente Header

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,16 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    --red: '#E52E4D';
-    --blue: '#5429CC';
+    --red: #E52E4D;
+    --blue: #5429CC;
 
-    --blue-light: '#6933FF';
+    --blue-light: #6933FF;
 
-    --text-title: '#363F5F';
-    --text-body: '#969CB3';
+    --text-title: #363F5F;
+    --text-body: #969CB3;
 
-    --background: '#F0F2F5';
-    --shape: '#FFFFFF';
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   * {
