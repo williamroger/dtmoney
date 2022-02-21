781e71373b8165631035c3c5a8eb1238bcd91fde
William Roger <williamroger.frontend@gmail.com>
2022-02-21 19:31:20 -0300

    formatando valores

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
+import { api } from '../../services/api';
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -15,17 +16,17 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
   const [value, setValue] = useState(0);
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
-  
-  const data = {
-    title,
-    value,
-    category,
-    type
-  };
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
-    console.log(data);
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data)
   }
 
   return (
